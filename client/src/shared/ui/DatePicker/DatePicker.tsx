67bbb17e7453589d1597fdc9b9f9efa8182616a7
Егор Ермолаев <probegrofanston@mail.ru>
2023-03-08 19:26:14 +0400

    Implemented shared module

---
+++ client/src/shared/ui/DatePicker/DatePicker.tsx
@@ -0,0 +1,55 @@
+import {memo, FC} from 'react'
+import {DatePicker as DatePickerMUI, LocalizationProvider} from '@mui/lab'
+import DateAdapter from '@mui/lab/AdapterDateFns'
+import {TextField, TextFieldProps} from '@mui/material'
+import ruLocale from 'date-fns/locale/ru'
+import enLocale from 'date-fns/locale/en-US'
+import {APP_LANG, DateUtils} from 'utils'
+
+interface Props {
+  value: string,
+  name: string,
+  setValue: (value: string, name: string) => void,
+  lang: APP_LANG,
+  minDate?: Date,
+  maxDate?: Date,
+}
+
+const localeMap = {
+  EN: enLocale,
+  RU: ruLocale,
+}
+
+const maskMap = {
+  EN: '__/__/____',
+  RU: '__.__.____',
+}
+
+export const DatePicker: FC<Props> = memo(({value, setValue, lang, name, minDate, maxDate}) => {
+  const handleChange = (value: Date | null) => {
+    const date = value ? DateUtils.getLocalDate(value) : DateUtils.getLocalDate(new Date())
+
+    setValue(date, name)
+  }
+
+  const renderInput = ({error, ...params}: TextFieldProps) => {
+    return (
+      <TextField className="datepicker" error={false} name={name} {...params} />
+    )
+  }
+
+  return (
+    <LocalizationProvider dateAdapter={DateAdapter} locale={localeMap[lang]}>
+      <DatePickerMUI<Date>
+        mask={maskMap[lang]}
+        value={value}
+        onChange={handleChange}
+        renderInput={renderInput}
+        minDate={minDate}
+        maxDate={maxDate}
+      />
+    </LocalizationProvider>
+  )
+})
+
+DatePicker.displayName = 'DatePicker'
---
+++ client/src/shared/ui/Input/Input.tsx
@@ -0,0 +1,71 @@
+import type {InputTypes} from 'model'
+import {memo, VFC, ChangeEvent} from 'react'
+import classNames from 'classnames'
+import {FilledInput, FormControlLabel} from '@mui/material'
+
+interface Props {
+  value: string,
+  name: string,
+  label: string,
+  placeholder: string,
+  type?: InputTypes,
+  className?: string,
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void,
+  error?: boolean | string,
+  readOnly?: boolean,
+}
+
+export const Input: VFC<Props> = memo(({
+  value,
+  name,
+  label,
+  type = 'string',
+  placeholder,
+  className,
+  onChange,
+  error,
+  readOnly,
+}) => {
+  const inputClassNames = classNames([
+    'input',
+    {[className ?? '']: !!className},
+  ])
+
+  const renderErrorMessage = () => {
+    if (typeof error === 'string') {
+      return (
+        <span className="input__error-message">
+          {error}
+        </span>
+      )
+    }
+
+    return null
+  }
+
+  return (
+    <div className={inputClassNames}>
+      <FormControlLabel
+        className="input__label"
+        control={
+          <FilledInput
+            readOnly={readOnly}
+            name={name}
+            className="input__area"
+            type={type}
+            placeholder={placeholder}
+            disableUnderline={true}
+            value={value}
+            onChange={onChange}
+          />
+        }
+        labelPlacement="top"
+        label={label}
+      />
+
+      {renderErrorMessage()}
+    </div>
+  )
+})
+
+Input.displayName = 'Input'
